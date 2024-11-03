ca58f2c3fb359b2ac25e1ff74c7b0897e2b29a14
joyosphere <joyosphere@gmail.com>
2024-11-03 22:35:25 +0900

    chore: pattern decomposition

--- scripts/1_joyo/decomposition.hs
+++ scripts/1_joyo/decomposition.hs
@@ -8,8 +8,15 @@ x2 :: Int
 xs :: TT
 (x1:x2:xs) = tt
 
+-- Recursive x:xs (x: 원소, xs: 나머지 배열) 로 분해를 통해 리스트 총 합을 구하는 예제
+sum' list = 
+    case list of
+        [] -> 0
+        x:xs -> x + sum' xs
+
 main :: IO ()
 main = do
     print x1
     print x2
     print xs
+    print $ sum' [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
