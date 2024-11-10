e4acb8dd6449e46846899c5b0ee9377e6af2c9a5
joyosphere <joyosphere@gmail.com>
2024-11-10 12:18:14 +0900

    chore: add section

--- scripts/2_joyo/insertIn.hs
+++ scripts/2_joyo/insertIn.hs
@@ -1,7 +1,7 @@
 insertIn :: a -> [a] -> Int -> [a]
 
 insertIn x ys 1 = x:ys
-insertIn x (y : ys) n = y : insertIn x ys (n-1)
+insertIn x (y:ys) n = y : insertIn x ys (n-1)
 insertIn x [] _ = [x]
 
 main :: IO ()
