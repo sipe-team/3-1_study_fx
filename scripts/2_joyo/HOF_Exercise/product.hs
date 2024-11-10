1278253cd8de564572f20aaf52adf069c3146be4
joyosphere <joyosphere@gmail.com>
2024-11-10 15:39:14 +0900

    chore: add hof exercise

---
+++ scripts/2_joyo/HOF_Exercise/product.hs
@@ -0,0 +1,24 @@
+
+--
+----
+------
+{-
+
+Problem | Implement a function prod :: [Int] -> Int that returns the product of a list of integers.
+Example | prod [2,10,5] = 100
+        | prod [3,1,2,4] = 24
+
+-}
+------
+----
+--
+
+prod :: [Int] -> Int
+
+prod = foldl (*) 1
+
+
+main :: IO ()
+main = do
+        print $ prod [2,10,5]
+        print $ prod [3,1,2,4]
