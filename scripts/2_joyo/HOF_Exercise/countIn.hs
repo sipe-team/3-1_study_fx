d427afd7bb02f07a1630ec775611e71ebc123b9d
joyosphere <joyosphere@gmail.com>
2024-11-11 19:32:54 +0900

    fix: countIn input type

--- scripts/2_joyo/HOF_Exercise/countIn.hs
+++ scripts/2_joyo/HOF_Exercise/countIn.hs
@@ -5,7 +5,7 @@
 {-
 
 Title   | countIn
-Problem | Implement a function countIn :: [Int] -> Int -> [Int] returns that reverses a list of integers
+Problem | Implement a function countIn :: [[Int]] -> Int -> [Int] returns that reverses a list of integers
 Example | countIn [[3,2,3], [3], [], [2,2]] 3 -- -> [2,1,0,0]
 
 -}
